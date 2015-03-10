de9db28e5ee42e21e64fc61076f2bc6797cf416b
Thibaut Xiong <thibaut.xiong@gmail.com>
2015-03-09 20:20:37 -0500

    added spread charts

--- pairsCase/timeseries.py
+++ pairsCase/timeseries.py
@@ -6,10 +6,18 @@ data = open("PairsRound2.csv", 'r')
 
 stock1 = []
 stock2 = []
+spread = []
+x = []
 
 for line in data:
-	stock1.append(line.split(',')[0])
-	stock2.append(line.split(',')[1])
+	#stock1.append(line.split(',')[0])
+	#stock2.append(line.split(',')[1])
+	stock1price = float(line.split(',')[0])
+	stock2price = float(line.split(',')[1])
+	spread.append(stock1price/stock2price)
+	x.append(1)
 
-plt.plot(stock1, '-', stock2, '-')
+#plt.plot(stock1, '-', stock2, '-')
+plt.plot(spread, '-')
+plt.plot(x, '-')
 plt.show()
