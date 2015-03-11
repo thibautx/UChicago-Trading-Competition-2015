1d14c07a0d690d071ef49bfb4041e6227c56b553
gfpastorek <gfpastorek@gmail.com>
2015-03-10 22:03:08 -0500

    making money on options FUCK YEAH

--- optionsCase/src/OptionsCase.java
+++ optionsCase/src/OptionsCase.java
@@ -3,6 +3,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
+import org.apache.commons.math3.analysis.differentiation.UnivariateDifferentiableFunction;
+import org.apache.commons.math3.analysis.solvers.BisectionSolver;
+import org.apache.commons.math3.analysis.solvers.NewtonRaphsonSolver;
+import org.apache.commons.math3.exception.DimensionMismatchException;
 import org.uchicago.options.OptionsHelpers.Quote;
 import org.uchicago.options.OptionsHelpers.QuoteList;
 import org.uchicago.options.OptionsMathUtils;
@@ -28,6 +34,8 @@ public class OptionsCase extends AbstractOptionsCase implements OptionsInterface
 
     final int strikes[] = {80, 90, 100, 110, 120};
 
+    double cash = 0;
+
     HashMap<Integer, Integer> positions = new HashMap<Integer, Integer>();
 
     EMA impliedVolEMA;
@@ -37,8 +45,8 @@ public class OptionsCase extends AbstractOptionsCase implements OptionsInterface
 
     @Override
     public void addVariables(IJobSetup setup) {
-        setup.addVariable("Alpha", "Aggressiveness: Number of standard deviations in vega to buffer quote against", "double", "1");
-        setup.addVariable("Xi", "Sensitivity to vega risk", "double", "1");
+        setup.addVariable("Alpha", "Passiveness: Number of standard deviations in vega to buffer quote against", "double", "1");
+        setup.addVariable("Xi", "Sensitivity to vega risk", "double", "5");
         setup.addVariable("EMA Decay", "Decay factor of the IV series EMA", "double", "0.5");
     }
 
@@ -61,26 +69,48 @@ public class OptionsCase extends AbstractOptionsCase implements OptionsInterface
 
     @Override
     public void newFill(int strike, int side, double price) {
-        log("Quote Fill, price=" + price + ", strike=" + strike + ", direction=" + side);
+        //log("Quote Fill, price=" + price + ", strike=" + strike + ", direction=" + side);
 
         /* update position */
         positions.put(strike, positions.get(strike) + side);
 
         /* estimate the true price by discounting the average edge our fills receive */
-        double truePrice = (side == 1) ? (price/0.95) : (price/1.05);
+        double truePrice = (side == -1) ? (price/0.95) : (price/1.05);
 
         /* compute IV via Dekker-Brent method */
         double lastVol = impliedVolatility(truePrice, strike);
 
+        //log("Compute lastVol = " + lastVol);
+
         /* add lastVol to IV EMA */
         impliedVolEMA.average(lastVol);
+
+        cash += side*price;
+
+        log("Got filled, immediate (lst) loss is " + side*(price-OptionsMathUtils.theoValue(strike, lastVol)));
+        log("Got filled, immediate (avg) loss is " + side*(price-OptionsMathUtils.theoValue(strike, impliedVolEMA.get())));
     }
 
     @Override
     public QuoteList getCurrentQuotes(){
-        log("My Case1 implementation received a request for current quotes");
+        //log("Received a request for current quotes");
 
         double totalVegaRisk = getTotalVegaRisk();
+
+        //log("Using IV = " + impliedVolEMA.get());
+
+        double assets = 0;
+
+        log("--------------");
+        log("Cash = " + cash);
+        for(int strike : strikes){
+            log(Integer.toString(strike) + " qty = " + positions.get(strike));
+            assets += positions.get(strike) * OptionsMathUtils.theoValue(strike, impliedVolEMA.get());
+        }
+        log("IV = " + impliedVolEMA.get());
+        log("Vega = " + getTotalVegaRisk());
+        log("PnL = " + (cash + assets));
+        log("--------------");
 
         Quote quoteEighty = getQuote(80, totalVegaRisk);
         Quote quoteNinety = getQuote(90, totalVegaRisk);
@@ -114,11 +144,12 @@ public class OptionsCase extends AbstractOptionsCase implements OptionsInterface
         double theoPrice = OptionsMathUtils.theoValue(strike, vol);
         double vegaRisk = getVegaRisk(strike);
         double delta = vegaRisk * volSD * alpha;
-        double omega = delta * totalVegaRisk * xi;
-        double bidPrice = theoPrice * (1 - delta + omega);
-        double askPrice = theoPrice * (1 + delta + omega);
-        bidPrice = 100;
-        askPrice = 110;
+        double omega = totalVegaRisk * xi;
+        double bidPrice = theoPrice * (1 - delta - Math.max(omega/5, omega));
+        double askPrice = theoPrice * (1 + delta - Math.min(omega/5, omega));
+        //log("Bid omega is " + -1*Math.max(omega/5, omega));
+        //log("Ask omega is " + -1*Math.min(omega/5, omega));
+        //log(Integer.toString(strike) + " quote is " + bidPrice + " - " + askPrice);
         return new Quote(strike, bidPrice, askPrice);
     }
 
@@ -142,9 +173,40 @@ public class OptionsCase extends AbstractOptionsCase implements OptionsInterface
 
     public double impliedVolatility(double price, double strike) {
 
-        IVSolver solver = new IVSolver(strike, price);
+        //IVSolver solver = new IVSolver(strike, price);
+        //double start = impliedVolEMA.get();
+        //return solver.solve(100000, 0.0, 5.0, start);
+        BisectionSolver solver = new BisectionSolver();
+        UnivariateDifferentiableFunction f = new ImpliedVolFunction(price, strike);
         double start = impliedVolEMA.get();
-        return solver.solve(10000, 0.0, 5.0, start);
+        return solver.solve(100000, f, 0.0, 5.0, start);
+        //NewtonRaphsonSolver solver = new NewtonRaphsonSolver();
+        //
+        //double start = impliedVolEMA.get();
+        //return solver.solve(100000, f, 0.0, 5.0, start);
+    }
+
+    class ImpliedVolFunction implements UnivariateDifferentiableFunction {
+
+        double strike;
+        double price;
+
+        public ImpliedVolFunction(double price, double strike){
+            this.strike = strike;
+            this.price = price;
+        }
+
+
+        @Override
+        public double value(double v) {
+            return OptionsMathUtils.theoValue(strike, v) - price;
+        }
+
+        @Override
+        public DerivativeStructure value(DerivativeStructure d) throws DimensionMismatchException {
+            return new DerivativeStructure(1, 1, OptionsMathUtils.calculateVega(strike, price));
+        }
+
     }
 
     class IVSolver  {
@@ -167,20 +229,26 @@ public class OptionsCase extends AbstractOptionsCase implements OptionsInterface
 
         public double solve(int max_count, double minVal, double maxVal, double start) {
 
-            double tolerance = .000001; // Our approximation of zero
+            double tolerance = 0.001; // Our approximation of zero
 
             double x = start;
+
+            log("Found vol of " + x + " with error of " + f(x) + " at count 0" + " where fprime = " + fprime(x) + " and price = " + price + " and strike = " + strike);
 
             for( int count=1;
                  (Math.abs(f(x)) > tolerance) && ( count < max_count);
                  count ++)  {
-                x= x - f(x)/fprime(x);
+                x = x - f(x)/fprime(x);
+                log("Found vol of " + x + " with error of " + f(x) + " at count " + count + " where fprime = " + fprime(x)  + " and price = " + price + " and strike = " + strike);
             }
+
+            log("Found vol of " + x + " with error of " + f(x));
 
             if( Math.abs(f(x)) <= tolerance) {
                 return x;
             }
             else {
+                log("Newton's method failed to converge");
                 return -1;
             }
         }
