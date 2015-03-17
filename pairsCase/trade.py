d069999bf7469baa276740f3925c6b2e4cf311bc
Thibaut Xiong <thibaut.xiong@gmail.com>
2015-03-16 19:06:50 -0500

    refactored, TODO: run trade.py and fix the position limits as well and also record the last trade (liquidate)

--- pairsCase/trade.py
+++ pairsCase/trade.py
@@ -1,24 +1,45 @@
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-#import csv
 
-global pnl, entry_spread, curr_spread, stock_spread, pos_multiple, position_limit
-position_limit = 40
-stock_spread = 1.0
+#global pnl, entry_spread, curr_spread, stock_spread, pos_multiple, pos_limit
+global pos_limit, pos_multiple, stock_spread, weights
+pos_limit = 40
+weights = [1.0, -1.0]
 pos_multiple = {"cash":0.0, "short":-1.0, "long":1.0}
-entry_spread = 0.0
-curr_spread = 0.0
-pnl = 0.0
+stock_spread = 1.0
 
-def ema(self, data, window):
-    if len(data) < 2 * window:
-        raise ValueError("data is too short")
-    c = 2.0 / (window + 1)
-    current_ema = self.sma(data[-window*2:-window], window)
-    for value in data[-window:]:
-        current_ema = (c * value) + ((1 - c) * current_ema)
-    return current_ema
+'''Returns spread, slow_mavg, fast_mavg, mavg_momentum'''
+def getData(data, mavg_windows):
+    slow_mavg_window = mavg_windows[0]
+    fast_mavg_window = mavg_windows[1]
+    momentum_window = mavg_windows[2]
+    spread = []
+    for line in data:
+        stock1price = float(line.split(',')[0])
+        stock2price = float(line.split(',')[1])
+        spread.append(stock1price - stock2price)
 
+    '''Indicators'''
+    slow_mavg = []
+    for i in xrange(1, slow_mavg_window):
+        slow_mavg.append(movingaverage(spread[:i],i))
+    slow_mavg.extend(movingaverage(spread, slow_mavg_window))
+
+    fast_mavg = []
+    for i in xrange(1, fast_mavg_window):
+        fast_mavg.append(movingaverage(spread[:i],i))
+    fast_mavg.extend(movingaverage(spread, fast_mavg_window))
+
+    momentum = movingAverageDeriv(spread)
+    mavg_momentum = []
+    for i in xrange(1, momentum_window):
+        mavg_momentum.append(movingaverage(momentum[:i],i))
+    mavg_momentum.extend(movingaverage(momentum, momentum_window))
+
+    mavgs = [slow_mavg, fast_mavg, mavg_momentum]
+    return spread, mavgs
+
+'''Indicators'''
 def movingaverage (data, window):
     ret = np.cumsum(data, dtype=float)
     ret[window:] = ret[window:] - ret[:-window]
@@ -30,94 +51,42 @@ def movingAverageDeriv(data):
         ret.append(10*(data[point+1]-data[point]))
     return ret
 
-#def signalGeneration():
-#    '''
-#    if more than $x away from the moving average, consider entry
-#    enter if moving average of momentum is zero
-#    exit upon stop loss or (reversion AND moving average of momentum is zero)
-#    '''
-#    print foo
-    
-def adjust_position( weights, initial, position, holdings ):
-    global pnl, entry_spread, curr_spread
-    if (initial == position): return holdings
-    n_pairs = position_limit / np.sum(np.abs(weights))
+'''Backtest & Signal Generation'''
+def adjustPosition( pnl, curr_spread, entry_spread, weights, initial, position, holdings ):
+    #global pnl, entry_spread, curr_spread
+    if (initial == position): return pnl, holdings
+    n_pairs = pos_limit / np.sum(np.abs(weights))
     desired_holdings = list(np.multiply( n_pairs*pos_multiple[position], weights ))
     change = desired_holdings 
     for k in xrange(len(holdings)):
         change[k] -= holdings[k]
-    
-#    print change
+    #print change
     pnl -= n_pairs*stock_spread
-#    print pnl
+    #print pnl
     if (position == "cash"):
         raw_trade_pnl = pos_multiple[initial]*(curr_spread-entry_spread) * n_pairs
-#        print "raw trade pnl", raw_trade_pnl
+        #print "raw trade pnl", raw_trade_pnl
         pnl += raw_trade_pnl
-        
-    return desired_holdings
     
-def covariance(self, window):
-    return 0
+    print desired_holdings
+    return pnl, desired_holdings
     
-
-if __name__ == "__main__":
-    #global pnl, entry_spread, curr_spread
-    data = open("PairsRound1.csv", 'r')
-    stock1 = []
-    stock2 = []
-    stock3 = []
-    spread = []
-    x = [] 
-    y = []
-    z = []
-
-    count = 0;
-    for line in data:
-        '''Charting the stocks'''
-        #stock1.append(line.split(',')[0])
-        #stock2.append(line.split(',')[1])
-        #stock3.append(line.split(',')[2])
-
-        '''Charting the spread'''
-        stock1price = float(line.split(',')[0])
-        stock2price = float(line.split(',')[1])
-        #stock3price = float(line.split(',')[2])
-        spread.append(stock1price - stock2price)
-        #ema.append()
-        x.append(0)
-        y.append(2)
-        z.append(-2)
-
-    
-    '''Indicators'''
-    window1 = 500
-    window2 = 50
-    window3 = 20
-
-    slow_mavg = []
-    for i in xrange(1, window1):
-        slow_mavg.append(movingaverage(spread[:i],i))
-    slow_mavg.extend(movingaverage(spread, window1))
-
-    fast_mavg = []
-    for i in xrange(1, window2):
-        fast_mavg.append(movingaverage(spread[:i],i))
-    fast_mavg.extend(movingaverage(spread, window2))
-
-    momentum = movingAverageDeriv(spread)
-    mavgmomentum = []
-    for i in xrange(1, window3):
-        mavgmomentum.append(movingaverage(momentum[:i],i))
-    mavgmomentum.extend(movingaverage(momentum, window3))
-    
-    '''Backtest'''
+def backtest(spread, mavgs, thresholds, pnlarray):
     holdings = [0.0, 0.0]
-    weights = [1.0, -1.0]
+    entries = [None]*1000
+    exits = [None]*1000
+    '''Parameters'''
     stdev_window = 20
-    entry_threshold = 6.0 #sigma
-    exit_threshold = 0.0 #sigma
-    risk_threshold = 15.0 #sigma
+    slow_mavg = mavgs[0]
+    fast_mavg = mavgs[1]
+    mavg_momentum = mavgs[2]
+
+    entry_threshold = thresholds[0]
+    exit_threshold = thresholds[1]
+    risk_threshold = thresholds[2]
+
+    entry_spread = 0.0
+    curr_spread = 0.0
     position = "cash"
     pnl = 0.0
     notional = 0.0
@@ -130,74 +99,123 @@ if __name__ == "__main__":
         diff = spread[k] - slow_mavg[k]
         initial = position
         if (position == "cash"):
-            if (diff >= entry_threshold*std >= 2.0*stock_spread and mavgmomentum[k] <= momentum_threshold):
+            if (diff >= entry_threshold*std >= 2.0*stock_spread and mavg_momentum[k] <= momentum_threshold):
                 entry_spread = spread[k]
                 position = "short"
+                entries[k] = entry_spread
                 print "cash -> short", k, curr_spread
-            elif (-diff >= entry_threshold*std >= 2.0*stock_spread and mavgmomentum[k] >= -momentum_threshold):
+
+            elif (-diff >= entry_threshold*std >= 2.0*stock_spread and mavg_momentum[k] >= -momentum_threshold):
                 entry_spread = spread[k]
                 position = "long"
+                entries[k] = entry_spread
                 print "cash -> long", k, curr_spread
-        
+
         elif (position == "short"):
-#            if (diff <= exit_threshold*std and mavgmomentum[k] >= momentum_threshold):
-#            if (curr_spread-entry_spread > 2.0*stock_spread and diff <= exit_threshold*std and mavgmomentum[k] >= momentum_threshold):
-#                position = "cash"
-#                print "short -> cash profit"
-            if (diff <= exit_threshold*std and mavgmomentum[k] >= momentum_threshold):
+            #if (diff <= exit_threshold*std and mavg_momentum[k] >= momentum_threshold):
+            #if (curr_spread-entry_spread > 2.0*stock_spread and diff <= exit_threshold*std and mavg_momentum[k] >= momentum_threshold):
+                #position = "cash"
+                #print "short -> cash profit"
+            if (diff <= exit_threshold*std and mavg_momentum[k] >= momentum_threshold):
                 position = "cash"
+                exits[k] = curr_spread
                 print "short -> cash profit", k, curr_spread
+
                 n_profitable += 1
-#            if (mavgmomentum[k] >= momentum_threshold):
-#                position = "cash"
-#                print "short -> cash profit"
+            #if (mavg_momentum[k] >= momentum_threshold):
+                #position = "cash"
+                #print "short -> cash profit"
             elif (diff >= risk_threshold*std):
                 position = "cash"
+                exits[k] = curr_spread
                 print "short -> cash loss", k, curr_spread
+
                 n_losses += 1
         
         elif (position == "long"):
-#            if (-diff <= exit_threshold*std and mavgmomentum[k] <= -momentum_threshold):
-#            if (curr_spread-entry_spread > 2.0*stock_spread and diff >= exit_threshold*std and mavgmomentum[k] <= -momentum_threshold):
-#                position = "cash"
-#                print "long -> cash profit"
-            if (diff >= exit_threshold*std and mavgmomentum[k] <= -momentum_threshold):
+            #if (-diff <= exit_threshold*std and mavg_momentum[k] <= -momentum_threshold):
+            #if (curr_spread-entry_spread > 2.0*stock_spread and diff >= exit_threshold*std and mavg_momentum[k] <= -momentum_threshold):
+                #position = "cash"
+                #print "long -> cash profit"
+            if (diff >= exit_threshold*std and mavg_momentum[k] <= -momentum_threshold):
                 position = "cash"
+                exits[k] = curr_spread
                 print "long -> cash profit", k, curr_spread
+
                 n_profitable += 1
-#            if (mavgmomentum[k] <= -momentum_threshold):
-#                position = "cash"
-#                print "long -> cash profit"
+            #if (mavg_momentum[k] <= -momentum_threshold):
+                #position = "cash"
+                #print "long -> cash profit"
             elif (-diff >= risk_threshold*std):
                 position = "cash" 
+                exits[k] = curr_spread
                 print "long -> cash loss", k, curr_spread
                 n_losses += 1
 
-#        print k, spread[k]
+        #print k, spread[k]
         if (k == len(spread)-1):
+            # liquidate position
+            print "liquidating positions"
             position = "cash"
             
-        holdings = adjust_position( weights, initial, position, holdings )
-        
+        pnl, holdings = adjustPosition( pnl, curr_spread, entry_spread, weights, initial, position, holdings )
+        #print holdings
         if (initial != position and position == "cash"):
             print "pnl:", pnl
-            
-#        print k, position, pnl
+
+        pnlarray.append(pnl)
+
+        #print k, position, pnl
         if (k == len(spread)-1):
             print "pnl:", pnl, "good trades", n_profitable, "bad trades", n_losses, "total trades", n_profitable+n_losses
 
+        
+    trades = [entries, exits]
+    return pnl, trades
 
-#    '''Plot Stocks'''
-#    fig, axes = plt.subplots(nrows=2)
-#    #plt.plot(stock1, '-', stock2, '-', stock3, '-')
-#    '''Plot spread'''
-#    h1, = plt.plot(spread, '-')
-#    #plt.plot(y, '-')
-#    #plt.plot(z, '-')
-#    #plt.plot(x, '-')
-#    '''Indicators'''
-#    h1, = plt.plot(slow_mavg, '-')
-#    h1, = plt.plot(fast_mavg, '-')
-#    h2, = plt.plot(mavgmomentum, '-')
-#    
-#    plt.show()
+#@TODO
+def generateBacktests():
+    print "foo"
+
+def generateThresholds():
+    thresholds = []
+    for i in xrange(0, 15):
+        for j in xrange(0, 5):
+            for k in xrange(0, 15):
+                thresholds.append([i, j, k])      
+
+#@TODO
+def covariance(self, window):
+    return 0
+    
+def graph(spread, mavgs, trades, pnlarray):
+    slow_mavg = mavgs[0]
+    fast_mavg = mavgs[1]
+    mavg_momentum = mavgs[2]
+    '''Plot'''
+    fig, axes = plt.subplots(nrows=3)
+    #plt.plot(stock1, '-', stock2, '-', stock3, '-')
+    axes[0].plot(trades[0], '^', ms = 10, color = 'r') # long
+    axes[0].plot(trades[1], 'v', ms = 10, color = 'g') # short
+    axes[0].plot(spread, '-')
+    axes[0].plot(slow_mavg, '-')
+    axes[0].plot(fast_mavg, '-')
+    axes[1].plot(mavg_momentum, '-')
+    axes[2].plot(pnlarray, '-')
+    plt.show()
+
+if __name__ == "__main__":
+    #global pnl, entry_spread, curr_spread
+    data = open("PairsRound1.csv", 'r')
+    
+
+    pnlarray = []
+    thresholds = [6.0, 0.0, 15.0] # entry_threshold, exit_threshold, risk_threshold
+    mavg_windows = [500, 50, 20] # [slow_mavg_window, fast_mavg_window, momentum_window]
+    spread, mavgs = getData(data, mavg_windows)
+    pnl, trades = backtest(spread, mavgs, thresholds, pnlarray)
+   # graph(spread, mavgs, trades, pnlarray)
+    
+
+
+
